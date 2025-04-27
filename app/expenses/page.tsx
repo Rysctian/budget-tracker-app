d827d56ea48e3eb89607f66f9ba992e37b76569a
ryschristian <creyes@schools.ph>
2025-04-28 00:08:11 +0800

    pages & charts added

---
+++ app/expenses/page.tsx
@@ -0,0 +1,6 @@
+
+export default function ExpenseRoute() {
+  return (
+    <div>ExpenseRoute</div>
+  )
+}
---
+++ app/income/page.tsx
@@ -0,0 +1,6 @@
+
+export default function IncomeRoute() {
+  return (
+    <div>IncomeRoute</div>
+  )
+}
